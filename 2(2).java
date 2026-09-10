ee30fc04f5470c0e618a17a13b0cd20fc1bbbe92
bhanuswetha428 <11249A428@kanchiuniv.ac.in>
2026-09-10 11:27:12 +0530

    Add UseMath

---
+++ 2(2).java
@@ -0,0 +1,7 @@
+import com.scsvmv.util.MathUtil;
+public class UseMath {
+public static void main(String[] args) {
+System.out.println("Square of 7 = " + MathUtil.square(7));
+System.out.println("Cube of 3 = " + MathUtil.cube(3));
+}
+}
